8e0f13d24fcd9f3048eb6e398c58bd6e627f703a
Nicolas FABRE <nico.fabre@gmail.com>
2016-11-25 10:41:18 +0100

    Update Transpose Matrix

--- src/main/java/com/comencau/utils/TransposeMatrixUtils.java
+++ src/main/java/com/comencau/utils/TransposeMatrixUtils.java
@@ -12,7 +12,7 @@ public class TransposeMatrixUtils {
     private static final Logger logger = LoggerFactory.getLogger(TransposeMatrixUtils.class);
 
     // Max number of bytes of the src file involved in each chunk
-    public static int MAX_BYTES_PER_CHUNK = 1024 * 100_000;// 50 MB
+    public static int MAX_BYTES_PER_CHUNK = 1024 * 10_000;// 10 MB
 
     public static File transposeMatrix(File srcFile, String separator) throws IOException {
         File output = File.createTempFile("output", ".txt");
@@ -20,37 +20,33 @@ public class TransposeMatrixUtils {
         return output;
     }
 
-    public static void transposeMatrix(File srcFile, File destFile, String separator) throws IOException {
-        long bytesPerColumn = assessBytesPerColumn(srcFile, separator);// rough assessment of bytes par column
+    public static void transposeMatrix(File src, File dest, String separator) throws IOException {
+        int nbColumns = getNumberOfColumns(src, separator);
+        long bytesPerColumn = src.length() / nbColumns;// rough assessment of bytes par column
         int nbColsPerChunk = (int) (MAX_BYTES_PER_CHUNK / bytesPerColumn);// number of columns per chunk according to the limit of bytes to be used per chunk
         if (nbColsPerChunk == 0) nbColsPerChunk = 1;// in case a single column has more bytes than the limit ...
-        logger.debug("file length : {} bytes. max bytes per chunk : {}. nb columns per chunk : {}.", srcFile.length(), MAX_BYTES_PER_CHUNK, nbColsPerChunk);
-        try (FileWriter fw = new FileWriter(destFile); BufferedWriter bw = new BufferedWriter(fw)) {
-            boolean remainingColumns = true;
-            int offset = 0;
-            while (remainingColumns) {
-                remainingColumns = writeColumnsInRows(srcFile, bw, separator, offset, nbColsPerChunk);
-                offset += nbColsPerChunk;
+        double nbChunks = Math.ceil((double) nbColumns / nbColsPerChunk);
+        logger.debug("file length : {} bytes. nb columns : {}. max bytes per chunk : {}. nb columns per chunk : {}. nb chunks : {}.",
+                src.length(), nbColumns, MAX_BYTES_PER_CHUNK, nbColsPerChunk, nbChunks);
+        try (FileWriter fw = new FileWriter(dest); BufferedWriter bw = new BufferedWriter(fw)) {
+            int n = 1;
+            for (int i = 0; i < nbColumns; i += nbColsPerChunk) {
+                int lastColumnIndex = Math.min(nbColumns, i + nbColsPerChunk);
+                logger.debug("chunk {} / {}. chunk for column {} to {} among {}", n, nbChunks, i, lastColumnIndex, nbColumns);
+                writeColumnsInRows(src, bw, separator, i, lastColumnIndex);
+                n++;
             }
         }
     }
 
-    private static boolean writeColumnsInRows(File srcFile, BufferedWriter bw, String separator, int offset, int nbColumns) throws IOException {
+    private static void writeColumnsInRows(File srcFile, BufferedWriter bw, String separator, int offset, int lastColumnIndex) throws IOException {
         List<String>[] newRows;
-        boolean remainingColumns = true;
         try (FileReader fr = new FileReader(srcFile); BufferedReader br = new BufferedReader(fr)) {
-            String[] split0 = br.readLine().split(separator);
-            if (split0.length <= offset + nbColumns) remainingColumns = false;
-            int lastColumnIndex = Math.min(split0.length, offset + nbColumns);
-            logger.debug("chunk for column {} to {} among {}", offset, lastColumnIndex, split0.length);
             newRows = new List[lastColumnIndex - offset];
-            for (int i = 0; i < newRows.length; i++) {
-                newRows[i] = new ArrayList<>();
-                newRows[i].add(split0[i + offset]);
-            }
+            for (int i = 0; i < newRows.length; i++) newRows[i] = new ArrayList<>();
             String line;
             while ((line = br.readLine()) != null) {
-                String[] split = line.split(separator);
+                String[] split = line.split(separator, -1);
                 for (int i = 0; i < newRows.length; i++) {
                     newRows[i].add(split[i + offset]);
                 }
@@ -64,13 +60,11 @@ public class TransposeMatrixUtils {
             }
             bw.newLine();
         }
-        return remainingColumns;
     }
 
-    private static long assessBytesPerColumn(File file, String separator) throws IOException {
+    private static int getNumberOfColumns(File file, String separator) throws IOException {
         try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
-            int nbColumns = br.readLine().split(separator).length;
-            return file.length() / nbColumns;
+            return br.readLine().split(separator).length;
         }
     }
 
